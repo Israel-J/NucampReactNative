3cc559ca9aebbaf64c99a2e7b3016777da0dd3d3
Israel Rdoriguez <israel355576@gmail.com>
2021-04-17 20:17:23 -0700

    Added the ability to post comments in the campsite directory

--- redux/comments.js
+++ redux/comments.js
@@ -5,8 +5,15 @@ export const comments = (state = { errMess: null, comments: []}, action) => {
         case ActionTypes.ADD_COMMENTS:
             return {...state, errMess: null, comments: action.payload};
 
+        case ActionTypes.ADD_COMMENT:
+            const newComment = action.payload
+            newComment.id = state.comments.length
+
+            return {...state, errMess: null, comments: [...state.comments, newComment]};
+
         case ActionTypes.COMMENTS_FAILED:
             return {...state, errMess: action.payload};
+
 
         default:
             return state;
