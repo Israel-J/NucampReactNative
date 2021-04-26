cc377ab024c243a282373c56eb4e14671f348990
Israel Rdoriguez <israel355576@gmail.com>
2021-04-26 14:39:24 -0700

    Added Secure Store and Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Modal, Alert } from 'react-native';
+import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
+
 
 class Reservation extends Component {
     constructor(props) {
@@ -17,10 +19,6 @@ class Reservation extends Component {
     static navigationOptions = {
         title: 'Reserver Campsite'
     }
-
-    // toggleModal() {
-    //     this.setState({showModal: !this.state.showModal});
-    // }
 
     handleReservation(){
         console.log(JSON.stringify(this.state));
@@ -39,15 +37,17 @@ class Reservation extends Component {
                 },
                 {
                     text: 'OK',
-                    onPress: () => this.resetForm()
-                },
+                    onPress: () => {
+                        this.presendLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                        this.resetForm();
+                    }
+                }  
             ],
             { cancelable: false }
             
           
 
         )
-        // this.toggleModal();
     }
 
         resetForm() {
@@ -58,6 +58,31 @@ class Reservation extends Component {
             showCalender: false,
             showModal: false
         });
+    }
+
+    async presendLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+        
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`          
+                },
+                trigger: null
+            });
+        }
+        let permissions = await Notifications.getPermissionsAsync();
+        if(!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if(permissions.granted) {
+            sendNotification();
+        }
     }
 
     
@@ -115,31 +140,8 @@ class Reservation extends Component {
                         onPress={() => this.handleReservation()}
                         title='Search'
                         color='#5637DD'
-                        // accessibilityLabel = 'Tap me to search for available campsites to reserve'
                     />
                 </View>
-                {/* <Modal 
-                    animationType={'slide'}
-                    transparent={false}
-                    visible={this.state.showModal}
-                    onRequestClose={() => this.toggleModal()}
-                >
-                    <View style={styles.modal}>
-                        <Text style={styles.modalTitle}>Search Campsite Reservations</Text>
-                        <Text style={styles.modalText}>Number of Campers: {this.state.campers}</Text>
-                        <Text style={styles.modalText}> Hike-In?: {this.state.hikeIn ? 'Yes' : 'No'}</Text>
-                        <Text style={styles.modalText}>Date: {this.state.date.toLocaleDateString('en-US')}</Text>
-                        <Button 
-                            onPress={() => {
-                                this.toggleModal();
-                                this.resetForm();
-                            }}
-                            color='#5637DD'
-                            title='Close'                
-        
-                        />
-                    </View>
-                // </Modal> */}
                 </Animatable.View>
         </ScrollView>
         );
